96c833b5619ee19848e617f3481358220844ecac
Philip Saa <cowglow@gmail.com>
2020-08-30 15:53:52 +0200

    Eye candy

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const RtmpServer = require("rtmp-server");
 
 const rtmpServer = new RtmpServer();
+const port = 1935;
 
-rtmpServer.on("error", err => {
+console.log("Start the RTMP streaming on your");
+console.log(" ______       ______        ");
+console.log("/ _____|     |  __  \\         ");
+console.log("| |  __  ___ | |__) |_______  ");
+console.log("| | |_ |/ _ \\|  ___/ '__/ _ \\");
+console.log("| |__| | (_) | |   | | | (_) |");
+console.log("|______|\\___/|_|   |_|  \\___/ ");
+console.log("                             ");
+console.log("     Hero 8 !!               ");
+console.log(" rtmp:[local.ip]:" + port + "/live");
+
+rtmpServer.on("error", (err) => {
   throw err;
 });
 
-rtmpServer.on("client", client => {
+rtmpServer.on("client", (client) => {
   client.on("connect", () => {
     console.log("connect", client.app);
   });
@@ -24,4 +36,4 @@ rtmpServer.on("client", client => {
   });
 });
 
-rtmpServer.listen(1935);
+rtmpServer.listen(port);
